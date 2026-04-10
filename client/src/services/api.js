d73e059974726f1855eb8e2ce0bcfbd76320b3a1
evalene20 <aklene20@gmail.com>
2026-04-10 12:03:39 +0530

    Add API service layer with axios instance

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,35 +1,7 @@
-// API service for backend communication
-// TODO: Implement actual API calls
+import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000',
+});
 
-const api = {
-  // Reports
-  getReports: async () => {
-    // TODO: Fetch all reports
-    console.log('Fetching reports from:', API_BASE_URL)
-  },
-  
-  createReport: async (reportData) => {
-    // TODO: Create new report
-    console.log('Creating report:', reportData)
-  },
-  
-  updateReport: async (id, updateData) => {
-    // TODO: Update existing report
-    console.log('Updating report:', id, updateData)
-  },
-  
-  deleteReport: async (id) => {
-    // TODO: Delete report
-    console.log('Deleting report:', id)
-  },
-  
-  // Statistics
-  getStats: async () => {
-    // TODO: Fetch dashboard statistics
-    console.log('Fetching statistics')
-  }
-}
-
-export default api
+export default api;
